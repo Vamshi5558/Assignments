938e872dbe13def15501897b3ca3b6516e3c037f
Vamshi Krishna Gannerla <g.vamshikrishna@yahoo.com>
2026-06-30 11:25:24 +0530

    Assignment 1

---
+++ javascript-training/print-statement.js
@@ -0,0 +1,7 @@
+//Generic statement to print
+console.log("Hello, world!");  
+
+//Debugging print statement
+console.info("This is an information message");
+console.warn(" This is a warning message");
+console.error(" This is an error message");
